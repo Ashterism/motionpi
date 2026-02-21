acdfee8ab8204c7e0856f42a90ab7d4c9632236a
Ash <33597520+Ashterism@users.noreply.github.com>
2026-02-21 19:41:32 +0400

    updating camera functions to resolve hanging on live

--- src/motionpi/camera.py
+++ src/motionpi/camera.py
@@ -4,6 +4,9 @@ from .storage import Storage
 storage = Storage()
 
 
+""" later on... move cam start from init to take image to save battery """
+
+
 class Camera:
 
     def __init__(self, mode="dev"):
@@ -11,14 +14,17 @@ class Camera:
         if self.mode == "prod":
             # setup for REAL camera
             from picamera2 import Picamera2
-
             self.cam = Picamera2()
 
-            ...
+            # Configure once for still capture and start camera
+            still_config = self.cam.create_still_configuration()
+            self.cam.configure(still_config)
+            self.cam.start()
 
         else:
             # set up mock
             ...
+
 
     def take_image(self):
         if self.mode == "prod":
@@ -40,6 +46,7 @@ class Camera:
             # Configure the camera for video capture before recording
             video_config = self.cam.create_video_configuration(main={"size": (1920, 1080)})
             self.cam.configure(video_config)
+            self.cam.start()
 
             encoder = H264Encoder(bitrate=8_000_000)
             output = FfmpegOutput(filepath)
@@ -47,6 +54,7 @@ class Camera:
             self.cam.start_recording(encoder, output)
             time.sleep(duration)
             self.cam.stop_recording()
+            self.cam.stop()
 
         elif self.mode == "dev":
             storage.create_mock_file("video")
